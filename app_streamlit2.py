413f20d0c8febb055b7e2ed80062a025172ff0f3
Rahil K <40102023+rahilkasimi@users.noreply.github.com>
2026-06-28 12:21:46 +0300

    Change TODAY_BG color code

--- app_streamlit2.py
+++ app_streamlit2.py
@@ -34,7 +34,7 @@ LIGHT_BLUE  = "#dce6f2"
 RED_DATE    = "#c00000"
 YELLOW      = "#f2ef00"
 WHITE       = "#ffffff"
-TODAY_BG    = "#ffd966"
+TODAY_BG    = "#F0EEC2"
 
 PRAYER_ORDER = ["FJ", "SR", "NN", "ZW", "MG"]
 
